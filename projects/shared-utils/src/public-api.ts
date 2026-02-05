81f38395dc24c3d525f6188de84a380da2e8a33d
sebastian <parra.sebastian91@gmail.com>
2026-02-05 15:49:06 -0300

    creacio nde shared-utils service y migracion de servicio theme, configuracion de arquitectura mfa ok

---
+++ projects/shared-utils/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of shared-utils
+ */
+
+export * from './lib/shared-utils.service';
+export * from './lib/shared-utils.component';
+export * from './lib/theme/theme.service';
+export * from './lib/theme/theme.interface';
+
